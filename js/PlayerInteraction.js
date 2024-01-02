361ccb74b096ba078dc46dad4d9a8cd737e6debc
IkedaAkihira <daiki.ikep.0729@gmail.com>
2024-01-02 17:47:52 +0900

    Add quiz functionality to player interaction

--- js/PlayerInteraction.js
+++ js/PlayerInteraction.js
@@ -3,6 +3,11 @@ let isBusy = true;
 
 const strikeButton = document.getElementById('strike');
 const heabyStrikeButton = document.getElementById('heavy-strike');
+const quizDialog = document.getElementById('quiz-dialog');
+const answerDialog = document.getElementById('answer-dialog');
+const showAnswerButton = document.getElementById('quiz-dialog-show-answer');
+const correctAnswerButton = document.getElementById('correct-answer');
+const wrongAnswerButton = document.getElementById('wrong-answer');
 
 strikeButton.addEventListener('click', () => {
     if (isBusy) {
@@ -16,4 +21,32 @@ heabyStrikeButton.addEventListener('click', () => {
         return;
     }
     actionCode = 'heavy-strike';
+});
+
+
+quizDialog.addEventListener('close', () => {
+    if (actionCode === '') {
+        actionCode = 'show-answer';
+    }
+});
+
+answerDialog.addEventListener('close', () => {
+    if (actionCode === '') {
+        actionCode = 'wrong-answer';
+    }
+});
+
+showAnswerButton.addEventListener('click', () => {
+    actionCode = 'show-answer';
+    quizDialog.close();
+});
+
+correctAnswerButton.addEventListener('click', () => {
+    actionCode = 'correct-answer';
+    answerDialog.close();
+});
+
+wrongAnswerButton.addEventListener('click', () => {
+    actionCode = 'wrong-answer';
+    answerDialog.close();
 });
